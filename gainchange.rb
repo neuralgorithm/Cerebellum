246319422cd506b01464420bf7d85aaef366fca5
YAMAZAKI Tadashi <github@neuralgorithm.org>
2012-03-18 10:47:34 +0900

    I changed the range of cycle number, because 0_16.dat does not exist.

--- gainchange.rb
+++ gainchange.rb
@@ -3,7 +3,7 @@
 initial = true
 initial_modulation = 1.0
 open("gainchange.dat", "w"){|o|
-  (0..300).step(10){|i|
+  (10..300).step(10){|i|
     system "./psth.rb #{i} 16"
     fn = "#{i}_16.dat"
     system "cp #{fn} tmp; gnuplot fit.gp > tmp.dat 2>&1"
